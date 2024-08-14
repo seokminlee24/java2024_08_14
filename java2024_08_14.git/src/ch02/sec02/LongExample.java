07bc87762142c66cec2cc7ebb1d0b230800584f5
seokminlee24 <lsm67286564@gmail.com>
2024-08-15 00:35:01 +0900

    변수

---
+++ java2024_08_14.git/src/ch02/sec02/LongExample.java
@@ -0,0 +1,14 @@
+package ch02.sec02;
+
+public class LongExample {
+    public static void main(String[] args) {
+        long var1 = 10;
+        long var2 = 20L;
+        //long var3 = 1000000000; 에러
+        long var3 = 1000000000L;
+
+        System.out.println(var1);
+        System.out.println(var2);
+        System.out.println(var3);
+    }
+}
