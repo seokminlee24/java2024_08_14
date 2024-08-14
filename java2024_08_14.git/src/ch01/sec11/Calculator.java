6978bfbb8620fe790195c6ed8b1faeb44d7c22a7
seokminlee24 <lsm67286564@gmail.com>
2024-08-14 22:25:29 +0900

    변수

---
+++ java2024_08_14.git/src/ch01/sec11/Calculator.java
@@ -0,0 +1,10 @@
+package ch01.sec11;
+
+public class Calculator {
+    public static void main(String[] args) {
+        int x = 1; int y =2; //변수 x,y 선언 변수값 저장
+        int result = x + y; //변수 result 선언 x와 y의 합은 result 저장
+
+        System.out.println(result); // 3
+    }
+}
