84002ae274c3f8d886f51f121a2e29084a4c77b8
seokminlee24 <lsm67286564@gmail.com>
2024-08-14 21:53:52 +0900

    Hello

---
+++ java2024_08_14.git/src/ch01/sec08/Hello1.java
@@ -0,0 +1,7 @@
+package ch01.sec08;
+
+public class Hello1 {//클래스명 = 소스파일명(Hello1)
+    public static void main(String[] args) {//메소드명
+        System.out.println("Hello, java"); //consol 뷰 출력
+    } //메소드 블록
+}//클래스 블록
