7a51133b052420ddac9cb43865844559b422353d
seokminlee24 <lsm67286564@gmail.com>
2024-08-16 22:02:07 +0900

    변수

--- java2024_08_14.git/src/ch02/sec07/PromotionExample.java
+++ java2024_08_14.git/src/ch02/sec07/PromotionExample.java
@@ -5,26 +5,26 @@ public class PromotionExample {
         //자동 타입 변환 int <-- byte
         byte byteValue = 10;
         int intValue = byteValue;
-        System.out.println("intValue = " + intValue);
+        System.out.println("intValue = " + intValue); // intValue = 10
         
         //자동 터입 변환 int <---- char
         char charValue = '가';
         intValue = charValue;
-        System.out.println("intValue = " + intValue);
+        System.out.println("intValue = " + intValue); //intValue = 44032
 
         //자동 타입 변환 long <--- int
         intValue = 50;
         long longValue = intValue;
-        System.out.println("longValue = " + longValue);
+        System.out.println("longValue = " + longValue);//longValue = 50
         
         //자동 타입 변환 float <---- long
         longValue = 100;
         float floatValue = longValue;
-        System.out.println("floatValue = " + floatValue);
+        System.out.println("floatValue = " + floatValue); //floatValue = 100.0
 
         //자동 타입 변환 double <--- float
         floatValue = 100.5f;
         double doubleValue = floatValue;
-        System.out.println("doubleValue = " + doubleValue);
+        System.out.println("doubleValue = " + doubleValue); //doubleValue = 100.5
     }
 }
