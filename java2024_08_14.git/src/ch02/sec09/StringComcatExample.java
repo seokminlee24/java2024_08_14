660eddc0a32c1a6ce5fca3f8189aa9814f93b9df
seokminlee24 <lsm67286564@gmail.com>
2024-08-17 01:17:38 +0900

    변수

--- java2024_08_14.git/src/ch02/sec09/StringComcatExample.java
+++ java2024_08_14.git/src/ch02/sec09/StringComcatExample.java
@@ -4,19 +4,19 @@ public class StringComcatExample {
     public static void main(String[] args) {
         //숫자 연산
         int result1 = 10 + 2 + 8;
-        System.out.println("result1 = " + result1);
+        System.out.println("result1 = " + result1); //result1 = 20
 
         //결합 연산
         String  result2 = 10 + 2 + "8";
-        System.out.println("result2 = " + result2);
+        System.out.println("result2 = " + result2); // 128
 
         String  result3 = 10 + "2" + 8;
-        System.out.println("result3 = " + result3);
+        System.out.println("result3 = " + result3); //1028
 
         String  result4 = "10" + 2 + 8;
-        System.out.println("result4 = " + result4);
+        System.out.println("result4 = " + result4); //1028
 
         String  result5 = "10" + (2 + 8);
-        System.out.println("result5 = " + result5);
+        System.out.println("result5 = " + result5); //1010
     }
 }
