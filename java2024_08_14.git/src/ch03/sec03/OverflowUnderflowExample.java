eae7f1a907d5d749a0f71a270adf4e30794cb776
seokminlee24 <lsm67286564@gmail.com>
2024-08-19 15:06:31 +0900

    정확한 계산은 정수 연산으로

---
+++ java2024_08_14.git/src/ch03/sec03/OverflowUnderflowExample.java
@@ -0,0 +1,17 @@
+package ch03.sec03;
+
+public class OverflowUnderflowExample {
+    public static void main(String[] args) {
+        byte var1 = 125;
+        for (int i = 0; i < 5; i++) { //{}을 5번 실행
+            var1++; //++ 연산은 var1의 값을 1증가 시킨다.
+            System.out.println("var1 = " + var1);
+        }
+        System.out.println("-------------------------");
+        byte var2 = -125;
+        for (int i = 0; i < 5; i++) { //{}을 5번 실행
+            var2--; //-- 연산은 var2의 값을 1감소 시킨다.
+            System.out.println("var2 = " + var2);
+        }
+    }
+}
