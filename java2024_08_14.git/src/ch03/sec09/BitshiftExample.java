a9ac9eae9cdd8ee01c965dd304a976c5817599fe
seokminlee24 <lsm67286564@gmail.com>
2024-08-19 23:16:10 +0900

    비트 이동 연산자

--- java2024_08_14.git/src/ch03/sec09/BitshiftExample.java
+++ java2024_08_14.git/src/ch03/sec09/BitshiftExample.java
@@ -7,5 +7,10 @@ public class BitshiftExample {
         int result2 = num1 * (int) Math.pow(2,3);
         System.out.println("result1 = " + result1);
         System.out.println("result2 = " + result2);
+        int num2 = -8;
+        int result3 = num2 >> 3; //오른쪽
+        int result4 = num2 / (int) Math.pow(2,3);
+        System.out.println("result3 = " + result3);
+        System.out.println("result4 = " + result4);
     }
 }
