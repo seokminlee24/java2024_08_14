c418758006aa4894c6469e77d83bcb854242ade5
seokminlee24 <lsm67286564@gmail.com>
2024-08-20 00:03:54 +0900

    비트 논리 연산자

--- java2024_08_14.git/src/ch03/sec08/BitLogicExample.java
+++ java2024_08_14.git/src/ch03/sec08/BitLogicExample.java
@@ -5,18 +5,18 @@ public class BitLogicExample {
     }
 
     public static void main(String[] args) {
-        System.out.println("45 & 25 = 9");
-        System.out.println("45 | 25 = 61");
-        System.out.println("45 ^ 25 = 52");
-        System.out.println("~45 = -46");
+        System.out.println("45 & 25 = 9"); //45 & 25 = 9
+        System.out.println("45 | 25 = 61"); //45 | 25 = 61
+        System.out.println("45 ^ 25 = 52"); //45 ^ 25 = 52
+        System.out.println("~45 = -46"); //~45 = -46
         System.out.println("-------------------------");
         byte receiveData = -120;
         int unsignedInt1 = receiveData & 255;
-        System.out.println(unsignedInt1);
+        System.out.println(unsignedInt1); //136
         int unsignedInt2 = Byte.toUnsignedInt(receiveData);
-        System.out.println(unsignedInt2);
+        System.out.println(unsignedInt2);//136
         int test = 136;
         byte btest = (byte)test;
-        System.out.println(btest);
+        System.out.println(btest);//-120
     }
 }
