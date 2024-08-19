5c19fa955450b4010cd5501b140fe515c40e0c1d
seokminlee24 <lsm67286564@gmail.com>
2024-08-20 00:04:11 +0900

    비트 이동 연산자

--- java2024_08_14.git/src/ch03/sec09/BitshiftExample.java
+++ java2024_08_14.git/src/ch03/sec09/BitshiftExample.java
@@ -5,12 +5,12 @@ public class BitshiftExample {
         int num1 =1;
         int result1 = num1 << 3; //왼쪽 이동
         int result2 = num1 * (int) Math.pow(2,3);
-        System.out.println("result1 = " + result1);
-        System.out.println("result2 = " + result2);
+        System.out.println("result1 = " + result1);//8
+        System.out.println("result2 = " + result2);//8
         int num2 = -8;
         int result3 = num2 >> 3; //오른쪽
         int result4 = num2 / (int) Math.pow(2,3);
-        System.out.println("result3 = " + result3);
-        System.out.println("result4 = " + result4);
+        System.out.println("result3 = " + result3);//-1
+        System.out.println("result4 = " + result4);//-1
     }
 }
--- java2024_08_14.git/src/ch03/sec09/BitshiftExample2.java
+++ java2024_08_14.git/src/ch03/sec09/BitshiftExample2.java
@@ -7,22 +7,22 @@ public class BitshiftExample2 {
         //우측으로 3byte 이동하고 띁 1바이트만 읽기
         byte byte1 = (byte) (value >>> 24);
         int int1 = byte1 & 255;
-        System.out.println("첫 번쨰 바이트 부호 없는 값 : " + int1);
+        System.out.println("첫 번쨰 바이트 부호 없는 값 : " + int1); //0
 
         //우측으로 2byte 이동하고 띁 1바이트만 읽기
         byte byte2 = (byte) (value >>> 16);
         int int2 = byte2 & 255;
-        System.out.println("두 번쨰 바이트 부호 없는 값 : " + int2);
+        System.out.println("두 번쨰 바이트 부호 없는 값 : " + int2); //0
 
         //우측으로 1byte 이동하고 띁 1바이트만 읽기
         byte byte3 = (byte) (value >>> 8);
         int int3 = byte3 & 255;
-        System.out.println("3 번쨰 바이트 부호 없는 값 : " + int3);
+        System.out.println("3 번쨰 바이트 부호 없는 값 : " + int3);//3
 
         //끝 1바이트만 읽기
         byte byte4 = (byte) value;
         int int4 = byte4 & 255;
-        System.out.println("두 번쨰 바이트 부호 없는 값 : " + int4);
+        System.out.println("두 번쨰 바이트 부호 없는 값 : " + int4);//4
     }
 }
 
