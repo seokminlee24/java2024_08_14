3a187410163c6c1ed1e347070f20db470f2cc085
seokminlee24 <lsm67286564@gmail.com>
2024-08-20 00:00:14 +0900

    부호/증감 연산자

--- java2024_08_14.git/src/ch03/sec01/IncreaseDecreaseOperatorExample.java
+++ java2024_08_14.git/src/ch03/sec01/IncreaseDecreaseOperatorExample.java
@@ -6,24 +6,25 @@ public class IncreaseDecreaseOperatorExample {
         int y = 10;
         int z;
 
-        x++;
-        ++x;
+        x++; //11
         System.out.println("x = " + x);
+        ++x; //12
+        System.out.println("x = " + x); //12
 
         System.out.println("--------------------------");
-        y--;
-        --y;
-        System.out.println("y = " + y);
+        y--; //9
+        --y; //8
+        System.out.println("y = " + y);//8
 
         System.out.println("--------------------------");
         z = x++;
-        System.out.println("z = " + z);
-        System.out.println("x = " + x);
+        System.out.println("z = " + z); //12
+        System.out.println("x = " + x); //13
 
         System.out.println("--------------------------");
         z = ++x + y++;
-        System.out.println("z = " + z);
-        System.out.println("x = " + x);
-        System.out.println("y = " + y);
+        System.out.println("z = " + z); //22
+        System.out.println("x = " + x);//14
+        System.out.println("y = " + y);//9
     }
 }
