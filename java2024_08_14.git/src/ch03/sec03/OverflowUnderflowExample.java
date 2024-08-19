6fb21666d675a9636d4953d276a57f3c18647783
seokminlee24 <lsm67286564@gmail.com>
2024-08-20 00:02:49 +0900

    오버플로우와 언더플로우

--- java2024_08_14.git/src/ch03/sec03/OverflowUnderflowExample.java
+++ java2024_08_14.git/src/ch03/sec03/OverflowUnderflowExample.java
@@ -7,11 +7,23 @@ public class OverflowUnderflowExample {
             var1++; //++ 연산은 var1의 값을 1증가 시킨다.
             System.out.println("var1 = " + var1);
         }
+        /*var1 = 126
+        var1 = 127
+        var1 = -128
+        var1 = -127
+        var1 = -126*/
         System.out.println("-------------------------");
         byte var2 = -125;
         for (int i = 0; i < 5; i++) { //{}을 5번 실행
             var2--; //-- 연산은 var2의 값을 1감소 시킨다.
             System.out.println("var2 = " + var2);
+
+        /*
+        * var2 = -126
+        var2 = -127
+        var2 = -128
+        var2 = 127
+        var2 = 126*/
         }
     }
 }
