c7c4ae59039cb44900db018b3c61686c8d92a52b
seokminlee24 <lsm67286564@gmail.com>
2024-08-20 00:02:33 +0900

    산술 연산자

--- java2024_08_14.git/src/ch03/sec02/ArithmeticOperatorExample.java
+++ java2024_08_14.git/src/ch03/sec02/ArithmeticOperatorExample.java
@@ -8,13 +8,13 @@ public class ArithmeticOperatorExample {
         long v4 = 10L;
 
         int result1 = v1 + v2; //모든 피연산자는 int 타입으로 자동변환
-        System.out.println("result1 = " + result1);
+        System.out.println("result1 = " + result1); //14
         long result2 = v1 + v2 - v4; // 모든 피연산자는 long 타입으로 자동변환 후 연산
-        System.out.println("result2 = " + result2);
+        System.out.println("result2 = " + result2); //4
         double result3 = (double)v1/v2; // double 타입 강제 변환 후 연산
-        System.out.println("result3 = " + result3);
+        System.out.println("result3 = " + result3);//2.5
 
         int result4 = v1 % v2;
-        System.out.println("result4 = " + result4);
+        System.out.println("result4 = " + result4);//2
     }
 }
